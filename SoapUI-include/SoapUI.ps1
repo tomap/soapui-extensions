92f48d897488c1dbae4f3a7d49373f1ac7dfadb3
tomap <piartt@gmail.com>
2018-01-17 16:06:36 +0100

    Update SoapUI to v5.4.0 to fix #1 (previous link was broken)

--- SoapUI-include/SoapUI.ps1
+++ SoapUI-include/SoapUI.ps1
@@ -4,7 +4,7 @@ $path = split-path $MyInvocation.MyCommand.Path
 
 if(!(Test-Path ($path + "\soapui"))){
 	
-	$urlZip = "http://cdn01.downloads.smartbear.com/soapui/5.3.0/SoapUI-5.3.0-windows-bin.zip"
+	$urlZip = "https://s3.amazonaws.com/downloads.eviware/soapuios/5.4.0/SoapUI-5.4.0-windows-bin.zip"
 	
 	$output = $path + "\soapui.zip"
 	Write-Host "Is: "
@@ -25,14 +25,14 @@ if(!(Test-Path ($path + "\soapui"))){
 	Write-Host "Unzip SoapUI..."
 
 	Unzip $output ($path + "\soapui")
-	$target = "soapui\SoapUI-5.3.0"
+	$target = "soapui\SoapUI-5.4.0"
 	Copy-Item soapui-settings.xml $target
 	
 }
 
 Write-Host "SoapUI is ready."
 
-$soapUiExe = $path + "\soapui\SoapUI-5.3.0\bin\testrunner.bat"
+$soapUiExe = $path + "\soapui\SoapUI-5.4.0\bin\testrunner.bat"
 $env:SOAPUI_EXE = $soapUiExe
 
 Write-Host("##vso[task.setvariable variable=SOAPUI_EXE;]$soapUiExe")
